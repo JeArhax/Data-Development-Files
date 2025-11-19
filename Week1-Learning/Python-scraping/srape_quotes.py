d65465671acc3aa2eca00ea2e4aa7acc81164265
JeArhax <cooljohnric24@gmail.com>
2025-11-19 15:21:31 +0800

    changes

--- Week1-Learning/Python-scraping/srape_quotes.py
+++ Week1-Learning/Python-scraping/srape_quotes.py
@@ -4,21 +4,18 @@ import csv
 
 r = requests.get('https://quotes.toscrape.com')
 r.encoding = 'utf-8'
-
 soup = BeautifulSoup(r.text, 'html.parser')
 
-quotes = soup.find_all('div', class_='quote')
+links = soup.find_all('div', class_='quote')
 
-formatted_quotes = []
+formatted_links = []
 
-for q in quotes:
-    text = q.find('span', class_='text').get_text(strip=True)
-    author = q.find('small', class_='author').get_text(strip=True)
-    formatted_quotes.append({
-        'text': text,
-        'author': author,
-       
-    })
+for link in links:
+    data = {
+        'text': link.find('span', class_='text').text,
+        'author': link.find('small', class_='author').text
+    }
+    formatted_links.append(data)
 
 # --- SAVE TO CSV ---
 csv_file = "quotes.csv"
@@ -26,6 +23,6 @@ csv_file = "quotes.csv"
 with open(csv_file, "w", newline="", encoding="utf-8") as file:
     writer = csv.DictWriter(file, fieldnames=["text", "author"])
     writer.writeheader()
-    writer.writerows(formatted_quotes)
+    writer.writerows(formatted_links)
 
 print("Saved to", csv_file)
